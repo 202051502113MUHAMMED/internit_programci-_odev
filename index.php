b48e7a4faa89f3de5a330382ef8ad72a6eb1218b
202051502113MUHAMMED <114560500+202051502113MUHAMMED@users.noreply.github.com>
2022-10-17 20:16:28 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+$sehir = [
+    "istanbol",
+    "adana",
+    "orfa",
+    "kahraman",
+    "dyarbekir "
+   
+];
+
+
+// odevi
+$i = 0;
+while( $i < count ($sehir))
+{
+    echo "<br>";
+    echo $sehir[$i];
+    $i++;
+}
+?>
