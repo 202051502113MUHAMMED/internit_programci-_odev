09e952227aa1be73bf8899f744555918c0acc121
202051502113MUHAMMED <114560500+202051502113MUHAMMED@users.noreply.github.com>
2022-10-26 21:45:16 +0300

    Add files via upload

---
+++ KutuphaneOtomasyonu/KutuphaneOtomasyonu/KutuphaneOtomasyonu/AnaEkran.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KutuphaneOtomasyonu
+{
+    public partial class AnaEkran : Form
+    {
+        public AnaEkran()
+        {
+            InitializeComponent();
+        }
+
+        private void ekleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            KitapEklemeEkrani kitapEklemeEkrani = new KitapEklemeEkrani(this);
+            kitapEklemeEkrani.Show();
+            this.Hide();
+        }
+
+        public void kitapEkle(string kitapAdi)
+        {
+            listBox1.Items.Add(kitapAdi);
+        }
+    }
+}
