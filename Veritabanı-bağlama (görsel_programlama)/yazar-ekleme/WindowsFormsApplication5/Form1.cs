974cfb0192196d59b289bc1234589ae7926a69c0
202051502113MUHAMMED <114560500+202051502113MUHAMMED@users.noreply.github.com>
2022-11-07 18:31:25 +0300

    Add files via upload

---
+++ Veritabanı-bağlama (görsel_programlama)/yazar-ekleme/WindowsFormsApplication5/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApplication5
+{
+    public partial class Form1 : Form
+    {
+        MySql.Data.MySqlClient.MySqlConnection conn;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            try
+            {
+                conn = new MySql.Data.MySqlClient.MySqlConnection();
+                conn.ConnectionString = "server=127.0.0.1;uid=root;pwd=;database=gorsel_programalama";
+                conn.Open();    
+
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            yazarListele();
+        }
+
+        public void yazarListele()
+        {
+
+            listBox1.Items.Clear();
+
+            string sql = "SELECT id, isim FROM yazarlar";
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            MySqlDataReader rdr = cmd.ExecuteReader();
+
+            while (rdr.Read())
+            {
+                listBox1.Items.Add(rdr[1]);
+
+            }
+            rdr.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sql = "INSERT INTO `yazarlar`(`isim`) VALUES ('"+ textBox1.Text +"')";
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            cmd.ExecuteNonQuery();
+
+            textBox1.Text = "";
+
+            yazarListele();
+        }
+    }
+}
--- Veritabanı-bağlama (görsel_programlama)/yazar-ekleme/WindowsFormsApplication5/Program.cs
+++ Veritabanı-bağlama (görsel_programlama)/yazar-ekleme/WindowsFormsApplication5/Program.cs
@@ -4,16 +4,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace KutuphaneOtomasyonu
+namespace WindowsFormsApplication5
 {
     static class Program
-
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new AnaEkran());
+            Application.Run(new Form1());
         }
     }
 }
